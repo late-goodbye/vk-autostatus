be231739652edc1b5a6d237ad6a0f864f1cc5b2a
Constantine <cmen.itis@gmailc.om>
2019-04-27 23:58:44 +0300

    update run.py

--- run.py
+++ run.py
@@ -3,12 +3,11 @@ import vk_api
 import time
 
 
-if __name__ == '__main__':
-    generator = Generator()
+generator = Generator()
 
-    while True:
-        vk = vk_api.VkApi(token=generator.token)
-        status = generator.generate_status()
-        vk.method("status.set", {"text": status})
-        print("Set status")
-        time.sleep(60)
+while True:
+    vk = vk_api.VkApi(token=generator.token)
+    status = generator.generate_status()
+    vk.method("status.set", {"text": status})
+    print("Set status")
+    time.sleep(60)
