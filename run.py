5c500623124907906a5d26c8772eee0662e58246
Constantine <cmen.itis@gmailc.om>
2019-04-28 00:55:00 +0300

    update status setting

--- run.py
+++ run.py
@@ -6,8 +6,10 @@ import time
 generator = Generator()
 
 while True:
-    vk = vk_api.VkApi(token=generator.token)
-    status = generator.generate_status()
-    vk.method("status.set", {"text": status})
-    print("Set status: {status}".format(status=status))
-    time.sleep(30)
+    if time.localtime().tm_sec == 0:
+        vk = vk_api.VkApi(token=generator.token)
+        status = generator.generate_status()
+        vk.method("status.set", {"text": status})
+        print("Set status: {status}".format(status=status))
+    else:
+        time.sleep(1)
